5533a756f16b01276f4a5bd5cbfb487f20401f6c
Liz C <ecolemansound@gmail.com>
2016-11-13 01:32:29 +0000

    initial commit

---
+++ meal_calculator.js
@@ -0,0 +1,20 @@
+// JavaScript File
+var diner = {
+    name: "",
+    var dishes = {
+        dish: [ ],
+        price: [ ],
+    },
+    var cost = 0;
+    for(var i=0; i<diner.dishes.price.length; i++)
+    cost += myArray[i],
+    tax: diner.cost*0.08875,
+    tip: diner.cost*0.2,
+};
+
+var bill = {
+    list: diners,
+    totalTax: diner.cost + diner.tax,
+    totalTip: diner.cost + diner.tip,
+    print: [diner.name, diner.cost, diner.tax, diner.tip],
+}
