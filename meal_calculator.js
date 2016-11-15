bbb44e332c422bf4dcb527441ffc3de752a4e84a
Liz C <ecolemansound@gmail.com>
2016-11-15 02:11:15 +0000

    second try

--- meal_calculator.js
+++ meal_calculator.js
@@ -6,15 +6,29 @@ var diner = {
         price: [ ],
     },
     var cost = 0;
-    for(var i=0; i<diner.dishes.price.length; i++)
+    for(var i=0; i<this.dishes.price.length; i++)
     cost += myArray[i],
-    tax: diner.cost*0.08875,
-    tip: diner.cost*0.2,
+    tax: this.cost*0.08875,
+    tip: this.cost*0.2,
 };
 
 var bill = {
-    list: diners,
-    totalTax: diner.cost + diner.tax,
-    totalTip: diner.cost + diner.tip,
-    print: [diner.name, diner.cost, diner.tax, diner.tip],
+    list: [ ],
+    totalTax: this.cost + this.tax,
+    totalTip: this.cost + this.tip,
+    print: [this.name, this.cost, this.tax, this.tip],
 }
+
+var Tom = object.create(diner);
+Tom.name = "Tom";
+Tom.dishes.dish = ["beans", "rice"];
+Tom.dishes.price = [2.25, 2];
+
+var Joan = object.create(diner);
+Joan.name = "Joan";
+Joan.dishes.dish = ["spinach", "salmon"];
+Joan.dishes.cost = [4.50, 10.00];
+
+bill.list.push('Tom', 'Joan');
+
+
